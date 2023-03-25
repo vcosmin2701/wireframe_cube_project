e16bc1bcb73db0cd5249d814462ecf177a17a1be
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-03-25 23:17:48 +0200

    New approach for this project

--- wireframe_project_OpenGL/wireframe_project_OpenGL/cub.cpp
+++ wireframe_project_OpenGL/wireframe_project_OpenGL/cub.cpp
@@ -5,41 +5,12 @@
 #include "glaux.h"
 
 static GLfloat alfaX = 0.0f;
-
-double vertices[8][3] = {
-    { 1.0f, 1.0f, 1.0f}, //0
-    { 1.0f, 1.0f,-1.0f}, //1
-    { 1.0f,-1.0f,-1.0f}, //2
-    {-1.0f,-1.0f,-1.0f}, //3
-    {-1.0f,-1.0f, 1.0f}, //4
-    {-1.0f, 1.0f, 1.0f}, //5
-    {-1.0f, 1.0f,-1.0f}, //6
-    { 1.0f,-1.0f, 1.0f}  //7
-};
-
-int indices[12][2] = {
-    {5,4},
-    {5,0},
-
-    {4,7},
-    {7,0},
-
-    {6,5},
-    {1,0},
-
-    {3,4},
-    {2,7},
-
-    {6,3},
-    {6,1},
-
-    {3,2},
-    {2,1}
-};
+static GLfloat alfaY = 0.0f;
+static GLfloat alfaZ = 0.0f;
 
 
 void myInit() {
-    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0);
 }
 
 void CALLBACK RotesteXStanga()
@@ -52,6 +23,27 @@ void CALLBACK RotesteXDreapta()
     alfaX = alfaX + 10;
 }
 
+
+void CALLBACK RotesteYStanga()
+{
+    alfaY = alfaY - 10;
+}
+
+void CALLBACK RotesteYDreapta()
+{
+    alfaY = alfaY + 10;
+}
+
+void CALLBACK RotesteZStanga()
+{
+    alfaZ = alfaZ - 10;
+}
+
+void CALLBACK RotesteZDreapta()
+{
+    alfaZ = alfaZ + 10;
+}
+
 void CALLBACK display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -60,19 +52,58 @@ void CALLBACK display()
     glTranslatef(0, 0, -500.0);
     glRotatef(30, 1.0, 1.0, 0.0);
     glRotatef(alfaX, 1.0, 0.0, 0.0);
+    glRotatef(alfaY, 0.0, 1.0, 0.0);
+    glRotatef(alfaZ, 0.0, 0.0, 1.0);
 
-    glColor3f(1, 0, 0);
-    glBegin(GL_LINES);
-    glBegin(GL_LINES);
-
-    for (int i = 0; i < 12; i += 1) {
-
-        glVertex3f(vertices[indices[i][0]][0], vertices[indices[i][0]][1], vertices[indices[i][0]][2]);
-        glVertex3f(vertices[indices[i][1]][0], vertices[indices[i][1]][1], vertices[indices[i][1]][2]);
-    }
-    glEnd();
-
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glColor3f(1.0f, 0.0f, 0.0f);
     
+    
+    glBegin(GL_TRIANGLES);
+    //Back
+    glVertex3f(100.0f, -100.0f, -100.0f);
+    glVertex3f(100.0f, 100.0f, -100.0f);
+    glVertex3f(-100.0f, -100.0f, -100.0f);
+    glVertex3f(-100.0f, -100.0f, -100.0f);
+    glVertex3f(-100.0f, 100.0f, -100.0f);
+    glVertex3f(100.0f, 100.0f, -100.0f);
+    //Front
+    glVertex3f(100.0f, -100.0f, 100.0f);
+    glVertex3f(100.0f, 100.0f, 100.0f);
+    glVertex3f(-100.0f, -100.0f, 100.0f);
+    glVertex3f(-100.0f, -100.0f, 100.0f);
+    glVertex3f(-100.0f, 100.0f, 100.0f);
+    glVertex3f(100.0f, 100.0f, 100.0f);
+    //Top
+    glVertex3f(100.0f, 100.0f, 100.0f);
+    glVertex3f(100.0f, 100.0f, -100.0f);
+    glVertex3f(-100.0f, 100.0f, 100.0f);
+    glVertex3f(-100.0f, 100.0f, 100.0f);
+    glVertex3f(-100.0f, 100.0f, -100.0f);
+    glVertex3f(100.0f, 100.0f, -100.0f);
+    //Bottom
+    glVertex3f(100.0f, -100.0f, 100.0f);
+    glVertex3f(100.0f, -100.0f, -100.0f);
+    glVertex3f(-100.0f, -100.0f, 100.0f);
+    glVertex3f(-100.0f, -100.0f, 100.0f);
+    glVertex3f(-100.0f, -100.0f, -100.0f);
+    glVertex3f(100.0f, -100.0f, -100.0f);
+    //Left
+    glVertex3f(100.0f, -100.0f, 100.0f);
+    glVertex3f(100.0f, -100.0f, -100.0f);
+    glVertex3f(100.0f, 100.0f, -100.0f);
+    glVertex3f(100.0f, 100.0f, -100.0f);
+    glVertex3f(100.0f, 100.0f, 100.0f);
+    glVertex3f(100.0f, -100.0f, 100.0f);
+    //Right
+    glVertex3f(-100.0f, -100.0f, 100.0f);
+    glVertex3f(-100.0f, -100.0f, -100.0f);
+    glVertex3f(-100.0f, 100.0f, -100.0f);
+    glVertex3f(-100.0f, 100.0f, -100.0f);
+    glVertex3f(-100.0f, 100.0f, 100.0f);
+    glVertex3f(-100.0f, -100.0f, 100.0f);
+
+    glEnd();
 
     glFlush();
 }
@@ -83,7 +114,7 @@ void CALLBACK myReshape(GLsizei w, GLsizei h)
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(2.0, (GLfloat)w / (GLfloat)h, 1.0, 1000.0);
+    gluPerspective(45.0, (GLfloat)w / (GLfloat)h, 1.0, 1000.0);
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -91,10 +122,16 @@ int main(int argc, char** argv)
 {
     auxInitDisplayMode(AUX_SINGLE | AUX_RGB);
     auxInitPosition(0, 0, 800, 600);
-    auxInitWindow("Un inceput de cub");
+    auxInitWindow("My CUBE");
     myInit();
     auxKeyFunc(AUX_q, RotesteXStanga);
-    auxKeyFunc(AUX_w, RotesteXDreapta);
+    auxKeyFunc(AUX_a, RotesteXDreapta);
+
+    auxKeyFunc(AUX_w, RotesteYStanga);
+    auxKeyFunc(AUX_s, RotesteYDreapta);
+
+    auxKeyFunc(AUX_e, RotesteZStanga);
+    auxKeyFunc(AUX_d, RotesteZDreapta);
 
     auxReshapeFunc(myReshape);
     auxMainLoop(display);
