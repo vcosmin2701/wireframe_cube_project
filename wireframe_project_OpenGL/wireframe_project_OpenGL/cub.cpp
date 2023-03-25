44e6abd1d115bd9b87673fec92172f8c821d9002
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-03-25 18:41:49 +0200

    Cube without triangularization

--- wireframe_project_OpenGL/wireframe_project_OpenGL/cub.cpp
+++ wireframe_project_OpenGL/wireframe_project_OpenGL/cub.cpp
@@ -6,6 +6,38 @@
 
 static GLfloat alfaX = 0.0f;
 
+double vertices[8][3] = {
+    { 1.0f, 1.0f, 1.0f}, //0
+    { 1.0f, 1.0f,-1.0f}, //1
+    { 1.0f,-1.0f,-1.0f}, //2
+    {-1.0f,-1.0f,-1.0f}, //3
+    {-1.0f,-1.0f, 1.0f}, //4
+    {-1.0f, 1.0f, 1.0f}, //5
+    {-1.0f, 1.0f,-1.0f}, //6
+    { 1.0f,-1.0f, 1.0f}  //7
+};
+
+int indices[12][2] = {
+    {5,4},
+    {5,0},
+
+    {4,7},
+    {7,0},
+
+    {6,5},
+    {1,0},
+
+    {3,4},
+    {2,7},
+
+    {6,3},
+    {6,1},
+
+    {3,2},
+    {2,1}
+};
+
+
 void myInit() {
     glClearColor(1.0, 1.0, 1.0, 1.0);
 }
@@ -30,14 +62,17 @@ void CALLBACK display()
     glRotatef(alfaX, 1.0, 0.0, 0.0);
 
     glColor3f(1, 0, 0);
-    glBegin(GL_QUADS);
-    {
-        glVertex3f(-100.0f, 100.0f, -100.0f);
-        glVertex3f(-100.0f, -100.0f, -100.0f);
-        glVertex3f(100.0f, -100.0f, -100.0f);
-        glVertex3f(100.0f, 100.0f, -100.0f);
+    glBegin(GL_LINES);
+    glBegin(GL_LINES);
+
+    for (int i = 0; i < 12; i += 1) {
+
+        glVertex3f(vertices[indices[i][0]][0], vertices[indices[i][0]][1], vertices[indices[i][0]][2]);
+        glVertex3f(vertices[indices[i][1]][0], vertices[indices[i][1]][1], vertices[indices[i][1]][2]);
     }
     glEnd();
+
+    
 
     glFlush();
 }
@@ -48,7 +83,7 @@ void CALLBACK myReshape(GLsizei w, GLsizei h)
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(45.0, (GLfloat)w / (GLfloat)h, 1.0, 1000.0);
+    gluPerspective(2.0, (GLfloat)w / (GLfloat)h, 1.0, 1000.0);
     glMatrixMode(GL_MODELVIEW);
 }
 
